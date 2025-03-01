1afd10a77401a77ab7bb00675924bba7958b92a5
Sherrrrryyy <Shaheerkhan090909@gmail.com>
2025-03-01 09:41:41 +0500

    feat: add global styles import to Dashboard component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import '../index.css';
+
 
 const Dashboard = () => {
   const navigate = useNavigate();
